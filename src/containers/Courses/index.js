87dfe1eeeea4f71d6d9335fa60bba3ac4925446d
lixue0825 <460830923@qq.com>
2017-11-14 12:20:25 +0800

    跑通路由和编写tab组件DOM结构

---
+++ src/containers/Courses/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+export default class Courses extends React.Component{
+    render(){
+        return(
+            <div>
+                Courses
+            </div>
+        )
+    }
+}
--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,10 +1,26 @@
 import React from "react";
+import {
+    HashRouter as Router,
+    Route,
+    NavLink
+} from "react-router-dom"
+import "./index.less"
+import Home from "../Home/index";
+import Courses from "../Courses/index";
+import Profile from "../Proflie/index";
+import Tab from "../../components/Tab/index";
 export default class App extends React.Component{
     render(){
         return(
+            <Router>
             <div>
-                app
+
+               <Route exact path="/" component={Home}/>
+               <Route path="/courses" component={Courses}/>
+               <Route path="/profile" component={Profile}/>
+                <Tab/>
             </div>
+            </Router>
         )
     }
 }
