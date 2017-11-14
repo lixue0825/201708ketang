63174b9d3b3bf892584039c6faadb05c40d2c4ac
lixue0825 <460830923@qq.com>
2017-11-14 12:34:22 +0800

    实现导航

--- src/components/Tab/index.js
+++ src/components/Tab/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import {NavLink} from "react-router-dom"
+import "./index.less"
 export default class Tab extends React.Component{
     render(){
         return(
             <nav className="tab">
-                <NavLink to="/"><i className="iconfont icon-xingqiiu"></i><span>首页</span></NavLink>
+                <NavLink exact to="/"><i className="iconfont icon-xingqiu"></i><span>首页</span></NavLink>
                 <NavLink to="/courses"><i className="iconfont icon-react"></i><span>我的课程</span></NavLink>
                 <NavLink to="/profile"><i className="iconfont icon-xiaolian"></i><span>个人中心</span></NavLink>
             </nav>
